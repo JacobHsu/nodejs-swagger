39028cff451504db33ed30469d50cdd83e9a4149
jacobhsu <jacob.hsu.tw@gmail.com>
2025-01-24 16:36:49 +0800

    feat: swagger-jsdoc swagger-ui-express

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ expressjs [installing](https://expressjs.com/en/starter/installing.html)
 [Hello world example](https://expressjs.com/en/starter/hello-world.html)  
 [Using template engines with Express](https://expressjs.com/en/guide/using-template-engines.html)
 
-[Postman](https://www.postman.com/): The World's Leading API Platform | Sign Up for Free
+[Postman](https://www.postman.com/): The World's Leading API Platform | Sign Up for Free  
+get https://nodejs-swagger-green.vercel.app/posts  
+post https://nodejs-swagger-green.vercel.app/posts  
+
+[swagger-jsdoc](https://www.npmjs.com/package/swagger-jsdoc)
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 var path = require("path");
+var cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 var indexRouter = require("./routes/index");
 var postRouter = require("./routes/posts");
 
 const app = express();
 const port = 3000;
+
+
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
@@ -15,8 +20,32 @@ const port = 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use("/", indexRouter);
 app.use("/posts", postRouter);
+
+
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      description: "A simple Express Library API",
+      version: "1.0.0",
+    },
+  },
+  apis: ["routes/*.js"], // files containing annotations as above
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { customCssUrl: CSS_URL })
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
