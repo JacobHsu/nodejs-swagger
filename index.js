348e6f6d865aab8a591e21beadfbf878f571e95a
jacobhsu <jacob.hsu.tw@gmail.com>
2025-01-24 21:47:06 +0800

    fix: index

--- index.js
+++ index.js
@@ -1,55 +1,50 @@
 import express from "express";
-import path from "path";
 import cors from "cors";
-import { fileURLToPath } from "url";
-import swaggerUi from "swagger-ui-express";
+import swaggerUI from "swagger-ui-express";
 import swaggerJsDoc from "swagger-jsdoc";
-import indexRouter from "./src/routes/index.js";
+
 import postRouter from "./src/routes/posts.js";
 
-const app = express();
-const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
-// 取得當前模組的目錄
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// view engine setup
-app.set("views", path.join(__dirname, "src", "views"));
-app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(cors());
-app.use("/", indexRouter);
-app.use("/posts", postRouter);
 
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
+const app = express();
+
+
+const PORT = process.env.PORT || 2001;
+
+app.use(cors());
 
 const options = {
   definition: {
     openapi: "3.0.0",
     info: {
       title: "Library API",
-      description: "A simple Express Library API",
       version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
     },
   },
-  apis: ["src/**/*.js"], // files containing annotations as above
+  // This is to call all the file
+  apis: ["src/**/*.js"],
 };
 
 const specs = swaggerJsDoc(options);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(
   "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(specs, { customCssUrl: CSS_URL })
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { customCssUrl: CSS_URL })
 );
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.use("/posts", postRouter);
+
+app.listen(PORT, () => console.log(`Server runs on port ${PORT}`));
