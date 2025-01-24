604989bcff9416d2d3ffb2a4e9daf8b81c71a9eb
jacobhsu <jacob.hsu.tw@gmail.com>
2025-01-24 15:24:05 +0800

    feat: pug

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 expressjs [installing](https://expressjs.com/en/starter/installing.html)  
 [Hello world example](https://expressjs.com/en/starter/hello-world.html)  
+[Using template engines with Express](https://expressjs.com/en/guide/using-template-engines.html)
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+var path = require("path");
+
+var indexRouter = require("./routes/index");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use("/", indexRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
